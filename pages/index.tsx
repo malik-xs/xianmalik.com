ac1962664316c5250e35b087d6bb474a4a068952
Malik Zubayer Ul Haider <zubayer@xpeedstudio.com>
2021-06-01 20:41:21 +0600

    Boilerplate Initialization

---
+++ pages/index.tsx
@@ -0,0 +1,52 @@
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import { getAllPosts } from '../lib/api'
+import Head from 'next/head'
+import { CMS_NAME } from '../lib/constants'
+import Post from '../types/post'
+import { Controller, Scene } from 'react-scrollmagic';
+
+type Props = {
+  allPosts: Post[]
+}
+
+const Index = ({ allPosts }: Props) => {
+  const heroPost = allPosts[0]
+  const morePosts = allPosts.slice(1)
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Next.js Blog Example with {CMS_NAME}</title>
+        </Head>
+        <Container>
+          <Controller>
+            <Scene duration={600} pin>
+              <div>Sticky Example</div>
+            </Scene>
+          </Controller>
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+export default Index
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
+}
